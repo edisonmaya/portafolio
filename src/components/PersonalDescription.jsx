6e0a7033cdad3a574b44476c1226ccae044d7c19
edisonmaya <ee_mayahuayamave2@hotmail.com>
2023-11-01 11:27:42 -0500

    modificaiones en PersonalDescription2

--- src/components/PersonalDescription.jsx
+++ src/components/PersonalDescription.jsx
@@ -2,10 +2,10 @@
 //md:text-4xl xl:text-5xl
 const PersonalDescription = () => {
     return (
-        <article id="PersonalDescription" className="max-w-[70vh] h-[100vh] my-auto lg:max-w-[85vh] p-8  flex flex-col gap-10 justify-end 2xl:justify-center text-white font-Sen font-bold break-words">
+        <article id="PersonalDescription" className="max-w-[70vh] h-[100vh] m lg:max-w-[85vh] p-8 mt-20 flex flex-col gap-10 justify-center text-white font-Sen font-bold break-words">
             <h2 className="text-2xl Mobile:text-3xl sm:text-4xl "><span className="text-[#9BA4ED] ">Hi! </span> I´m a FullStack<p>Web Developed.</p></h2>
             <p className=" font-normal text-justify text-xs Mobile:text-sm md:text-md lg:text-lg xl:text-xl  ">I’m probably the most passionate developer you will ever get to work with. If you have a great project that needs some amazing skills, I’m your guy.</p>
-            <button className="invisible w-max px-5 py-10 border-2 text-sm  font-normal rounded-lg xl:text-xl">Click here</button>
+            <button className="hidden w-max px-5 py-10 border-2 text-sm  font-normal rounded-lg xl:text-xl">Click here</button>
         </article>
     )
 }
